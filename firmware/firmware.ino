f90c11dd83caaec643506a23d988dec5b9ee0543
PecceG2 <peccettog@live.com>
2019-05-18 22:25:30 -0300

    [+] Método para generar y devolver solcitudes HTML [+] Recibir y leer solicitudes de clientes via webserver [*] Mensajes de debug en caso de errores de conexion [+] Reconexion en caso de perder conectividad con la red

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -6,8 +6,8 @@ const char* ssid = "TestNetwork";
 const char* passwd = "12345678";
 const boolean debug = true;
 const int serialSpeed = 9600;
-const char* ProgramVersion = "0.0.1 - e2b8fb3";
-WiFiServer serverip(80);
+const String ProgramVersion = "0.0.1 - 58fa97d";
+WiFiServer tcpserver(80);
 
 /* 
  *  DEBUG FUNCTION
@@ -38,12 +38,33 @@ void setup(){
   
   //Wake up methods
   connectWiFi();
+  tcpserver.begin();
 
 }
 
 void loop(){
-  Serial.println("LOOP");
-  delay(4000);
+  if(checkConnection()){
+    WiFiClient client = tcpserver.available();
+    if(client){
+      sendDebug('i', "[SERVER] New client connected.");
+      sendDebug('i', "[SERVER] Client IP: "+client.remoteIP().toString());
+      while (client.connected()){
+      // Get client request
+        if (client.available()){
+          String line = client.readStringUntil('\r');
+          sendDebug('i', line);
+          
+          if(line.length() == 1 && line[0] == '\n'){
+            client.println(htmlGenerator(200, 1, 1));
+            break;
+          }
+        }
+      }
+    }
+  }else{
+    sendDebug('e', "Lost connection after started! Retry connection to WiFi network...");
+    connectWiFi();
+  }
 }
 
 
@@ -52,11 +73,12 @@ void connectWiFi(){
     WiFi.begin(ssid, passwd);
     sendDebug('i', "Connecting to WiFi network");
     while(WiFi.status() != WL_CONNECTED && WiFi.status() != WL_CONNECT_FAILED){
-      Serial.println(WiFi.status());
+      sendDebug('i', ".");
       delay(500);
     }
     if(WiFi.status() == WL_CONNECTED){
       sendDebug('i', "Connected correctly.");
+      sendDebug('i', "IP: "+WiFi.localIP().toString());
     }else if(WiFi.status() == WL_CONNECT_FAILED){
       sendDebug('e', "Error connecting to the network. (¿SSID out of range?)");
     }else{
@@ -65,4 +87,46 @@ void connectWiFi(){
   }else{
     sendDebug('w', "Connection request canceled. The WiFi module is already connected to a network.");
   }
+}
+
+String htmlGenerator(int header, int htmlheader, int htmlbody){
+  String header200 = "HTTP/1.1 200 OK\r\n Content-Type: text/html\r\n Connection: close\r\n";
+  String header500 = "HTTP/1.1 500";
+  String basicHtmlHeader = "<html><head><title>ESP8266-Simple-IoT</title></head>";
+  String basicHtmlBody = "<body><h2>Welcome to ESP8266-Simple-IoT</h2><h4> v"+ ProgramVersion +"</h4>";
+  String htmlGenerated = "";
+  switch(header){
+    case 200:
+      htmlGenerated += header200;
+    break;
+    case 500:
+    default:
+      htmlGenerated += header500;
+    break;
+  }
+
+  switch(htmlheader){
+    case 1:
+    default:
+      htmlGenerated += basicHtmlHeader;
+    break;
+  }
+
+  switch(htmlbody){
+    case 1:
+    default:
+      htmlGenerated += basicHtmlBody;
+    break;
+  }
+}
+
+//void sendBrowserResponse(String response){
+//  client.println(completeResponse);
+//}
+
+boolean checkConnection(){
+  if(WiFi.status() != WL_CONNECTED){
+    return false;
+  }
+  return true;
 }
